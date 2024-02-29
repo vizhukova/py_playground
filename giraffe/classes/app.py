fd05c8b4ef6146eae69655b7bcafcf278d6b2ef2
Veronika Zhukova <veronikazhukova9@gmail.com>
2024-02-29 13:45:37 +0200

    simple file's change

--- giraffe/classes/app.py
+++ giraffe/classes/app.py
@@ -1,7 +1,22 @@
 from student import Student
 
-student1 = Student("Alex", "Business", 3.1, False)
-student2 = Student("Vera", "Art", 4.5, True)
+student1 = Student("Alex", 12, 1000)
+student2 = Student("Vera", 15, 900)
 print(student1)
 print(student2)
 
+student1.show()
+
+# call class method using the class
+Student.change_School('XYZ School')
+# call class method using the object
+student1.change_School('PQR School')
+
+# call static method using the class
+Student.find_notes('Math')
+# call class method using the object
+student1.find_notes('Math')
+
+print(student1.name)
+# returns error as param is private
+# print(student1.__salary)
--- giraffe/classes/student.py
+++ giraffe/classes/student.py
@@ -1,6 +1,29 @@
 class Student:
-    def __init__(self, name, major, gpa, is_on_probation):
+    '''documentation string'''
+
+    # class variables
+    school_name = 'ABC School'
+
+    def __init__(self, name: str, age: int, salary: float):
+        # instance variables
         self.name = name
-        self.major = major
-        self.gpa = gpa
-        self.is_on_probation = is_on_probation
+        self.age = age
+        # private member
+        self.__salary = salary
+
+    def show(self):
+        print(self.name, self.age, Student.school_name)
+
+    @classmethod
+    def change_School(cls, name: str):
+        cls.school_name = name
+
+    @staticmethod
+    def find_notes(subject_name: str) -> int:
+        try:
+            index = ['chapter 1', 'chapter 2', 'chapter 3', 'Math'].index(subject_name)
+            print(f'Note {subject_name} is found on index {index}')
+            return index
+        except:
+            print('No such note')
+            return None
